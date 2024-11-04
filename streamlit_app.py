93e1cadb40304faf01e6c031e352900675181511
bsmith6kids <bsmith6kids@gmail.com>
2024-11-04 15:25:17 -0600

    Update streamlit_app.py
    connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
-
  
 # Write directly to the app
 st.title("Customize Your Smoothie! :balloon:")
@@ -12,8 +10,6 @@ st.write(
 )
 
 # Get the current credentials
- 
- 
 cnx = st.connection("snowflake")
 session = cnx.session()
  
